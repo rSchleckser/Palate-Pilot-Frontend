618d015640fa06be594efb283dcd95d483ecb3f9
Farid Virani <fvirani786@gmail.com>
2024-07-17 23:50:27 -0500

    Feat:updated changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,64 +1,28 @@
-// src/App.jsx
-import React from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-import Card from './components/Card';
+import React, { useEffect, useState } from 'react';
+import Button from './components/Buttons';
 
-const App = () => {
-  const countries = [
-    { 
-      country: "India", 
-      facts: "India is the seventh-largest country by land area.",
-      dish: {
-        name: "Butter Chicken",
-        image: "/images/butter-chicken.jpg",
-        description: "A rich and creamy dish with a blend of spices.",
-        review: "Delicious and flavorful!"
-      }
-    },
-    {
-      country: "China",
-      facts: "China is the most populous country in the world.",
-      dish: {
-        name: "Peking Duck",
-        image: "/images/peking-duck.jpg",
-        description: "A famous duck dish known for its crispy skin.",
-        review: "Crispy and flavorful!"
-      }
-    },
-    {
-      country: "Japan",
-      facts: "Japan is an island country in East Asia.",
-      dish: {
-        name: "Sushi",
-        image: "/images/sushi.jpg",
-        description: "A traditional Japanese dish with vinegared rice.",
-        review: "Fresh and exquisite!"
-      }
-    }
-  ];
+function App() {
+  const [message, setMessage] = useState('');
+  useEffect(() => {
+    fetch('/')
+      .then((res) => res.text())
+      .then((data) => setMessage(data))
+      .catch((err) => console.log(err));
+  }, []);
 
-  return (
-    <Router>
+
+  const handleClick = () => {
+    console.log('Button has been clicked!');
+  };
+
+  return ( 
+    <>
       <div>
-        <h1>Select a Continent</h1>
-        <nav>
-          <ul>
-            <li><Link to="/asia">Asia</Link></li>
-          </ul>
-        </nav>
-
-        <Switch>
-          <Route path="/asia">
-            <h2>Countries in Asia</h2>
-            {countries.map((countryData, index) => (
-              <Card key={index} country={countryData.country} facts={countryData.facts} dish={countryData.dish} />
-            ))}
-          </Route>
-        </Switch>
+        <h1>PalatePilot</h1>
+        <Button buttonName={'CLICK HERE!'} onClick={handleClick} style={{ backgroundColor: '#6499E9', color: '#BEFFF7' }}></Button>
       </div>
-    </Router>
+    </>
   );
-};
+}
 
 export default App;
-
