a9fd3c441e22041ee2e98d51f2b52d63e7ad2608
itspancakeman <collinjst.onge@gmail.com>
2024-07-17 23:09:09 -0400

    feat: connected front-end to back-end!

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,20 @@
+import React, { useEffect, useState } from 'react';
+
 function App() {
-  return <></>;
+  const [message, setMessage] = useState('');
+  useEffect(() => {
+    fetch('/')
+      .then((res) => res.text())
+      .then((data) => setMessage(data))
+      .catch((err) => console.log(err));
+  }, []);
+  return ( 
+    <>
+      <div>
+        <h1>{message}</h1>
+      </div>
+    </>
+  );
 }
 
 export default App;
