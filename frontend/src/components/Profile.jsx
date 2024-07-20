066d031b6ef13f4c019b70ebef95ecf7d4f66aac
itspancakeman <collinjst.onge@gmail.com>
2024-07-20 15:41:26 -0400

    feat: updated profile to include favorites and navbar components and pull user data from db

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,27 +1,42 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import Favorites from "./Favorites";
+import Navbar from "./Navbar";
+import axios from 'axios';
 
 const Profile = () => {
+
+    const [user, setUser] = useState({})
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/users')
+        .then(res => {
+            setUser(res.data);
+        })
+        .catch(err => {
+            console.log('error: ', err);
+        })
+    });
     
     
     return(
         <>
             <div>
                 <div>
-                    {/*Navbar component here*/}
+                    <Navbar />
                 </div>
 
                 <div>
                     <h1>Welcome to your profile!</h1>
                 </div>
                 <div>
-                    <h3>Username: ${req.body.username}</h3>
-                    <h3>Email: ${req.body.email}</h3>
+                    <h3>Username: ${user.username}</h3>
+                    <h3>Email: ${user.email}</h3>
                 </div>
                 <div>
                     {/*Profile pic here if time allows*/}
                 </div>
                 <div>
-                   {/*Favorites component here*/}
+                   <Favorites />
                 </div>
                 <dov>
                     {/*Reviews component here*/}
