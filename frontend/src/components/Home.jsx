a333d953705189bcef4c2b7e8362c2bb371e5a29
Anwar-Abdel <anwarabdeljaber0@gmail.com>
2024-07-22 18:29:38 -0500

    feat: home component

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,17 @@
+import Navbar from "./Navbar";
+// import Card from "./Card";
+// import Favorites from "./Favorites";
+
+const Home = () => {
+    // const [user, setUser] = useState({});
+
+    return (
+        <div>
+            <Navbar />
+            {/* <Favorites />
+            <Card /> */}
+        </div>
+    );
+}
+
+export default Home;
