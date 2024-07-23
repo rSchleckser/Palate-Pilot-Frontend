b8e3e4bb372670a0f6e756f8f080f3b44c92c0d6
rSchleckser <rickyschleckser@gmail.com>
2024-07-22 19:16:22 -0700

    feat: add additional error handling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,16 @@
-import { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 // import ReviewPage from './components/ReviewDish'; // Ensure this import is correct
 import Login from './components/Login';
 import SignUp from './components/signUp';
+import Review from './components/Review';
 // import Cards from './components/Card'
 import './App.css';
 
 function App() {
-  const [data, setData] = useState([]);
-
   // const handleClick = () => {
   //   console.log('Button has been clicked!');
   // };
-
-  useEffect(() => {
-    fetch('/api/data') // Adjust the endpoint if needed
-      .then((res) => res.json())
-      .then((data) => setData(data))
-      .catch((err) => console.log(err));
-  }, []);
 
   return (
     <>
@@ -27,6 +18,7 @@ function App() {
         <Route path='/' element={<Navbar />} />
         <Route path='/auth/signup' element={<SignUp />} />
         <Route path='/auth/login' element={<Login />} />
+        <Route path='/reviews' element={<Review />} />
       </Routes>
 
       <div>
@@ -34,13 +26,6 @@ function App() {
               CLICK HERE!
             </button> */}
 
-        <div>
-          {data.length > 0 ? (
-            <div>{JSON.stringify(data)}</div> // Displaying data, adjust as needed
-          ) : (
-            <p>Loading data...</p>
-          )}
-        </div>
         <div className='container'>
           <h2>Select a Continent</h2>
           {/* <div className="card-container">
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,37 +1,35 @@
-import { CgProfile } from "react-icons/cg";
-import logo from '../images/logo.png';
-import '../css/Navbar.css'
-import axios from "axios";
-import { useEffect } from "react";
+// import { CgProfile } from "react-icons/cg";
+// import logo from '../images/logo.png';
+// import '../css/Navbar.css'
+// import axios from "axios";
+// import { useEffect } from "react";
 
+// const Navbar = () => {
 
+//     useEffect(() => {
+//         axios.get('http://localhost:3000/favorites')
+//         .then(res => {
+//             setFavorites(res.data);
+//         })
+//         .catch(err => {
+//             console.log('error: ', err);
+//         })
+//     });
 
-const Navbar = () => {
+//     return(
+//         <>
+//         <div>
+//             <img src={ logo } height={300} width={150}/>
+//         </div>
+//         <div>
+//             <h1>Welcome to PalatePilot!</h1>
+//         </div>
+//         <div id="userIcon">
+//             <CgProfile />
+//             <h6>Username</h6>
+//         </div>
+//         </>
+//     )
+// }
 
-    useEffect(() => {
-        axios.get('http://localhost:3000/favorites')
-        .then(res => {
-            setFavorites(res.data);
-        })
-        .catch(err => {
-            console.log('error: ', err);
-        })
-    });
-    
-    return(
-        <>
-        <div>
-            <img src={ logo } height={300} width={150}/>
-        </div>
-        <div>
-            <h1>Welcome to PalatePilot!</h1>
-        </div>
-        <div id="userIcon">
-            <CgProfile />
-            <h6>Username</h6>
-        </div>
-        </>
-    )
-}
-
-export default Navbar;
+// export default Navbar;
