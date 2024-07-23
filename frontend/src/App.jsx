32f3ac06b52b989dc24081ee785a9d90aab8dcbd
Anwar-Abdel <anwarabdeljaber0@gmail.com>
2024-07-22 19:13:15 -0500

    feat: moved signup and login buttons to home component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes, useNavigate } from 'react-router-dom';
+import { Route, Routes} from 'react-router-dom';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import Login from './components/Login';
@@ -7,15 +7,6 @@ import Review from './components/Review';
 import './App.css';
 
 function App() {
-  const navigate = useNavigate();
-
-  const handleLoginClick = () => {
-    navigate('/auth/login');
-  };
-
-  const handleSignupClick = () => {
-    navigate('/auth/signup');
-  };
 
   return (
     <>
@@ -26,18 +17,6 @@ function App() {
         <Route path='/auth/login' element={<Login />} />
         <Route path='/reviews' element={<Review />} />
       </Routes>
-
-      <div>
-        <button className="login-button" onClick={handleLoginClick}>
-          Login here
-        </button>
-      </div>
-
-      <div>
-        <button className="signup-button" onClick={handleSignupClick}>
-          Signup here
-        </button>
-      </div>
     </>
   );
 }
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,16 +1,42 @@
 //import Navbar from "./Navbar";
 // import Card from "./Card";
 // import Favorites from "./Favorites";
+import { useNavigate } from 'react-router-dom';
+
 
 const Home = () => {
-    // const [user, setUser] = useState({});
+
+    const navigate = useNavigate();
+
+    const handleLoginClick = () => {
+        navigate('/auth/login');
+    };
+
+    const handleSignupClick = () => {
+        navigate('/auth/signup');
+    };
 
     return (
         <div>
             <h1>Welcome to Palate Pilot!</h1>
             {/* <Favorites />
             <Card /> */}
+
+            <div>
+                <button className="login-button" onClick={handleLoginClick}>
+                    Login here
+                </button>
+            </div>
+
+            <div>
+                <button className="signup-button" onClick={handleSignupClick}>
+                    Signup here
+                </button>
+            </div>
+
         </div>
+
+
     );
 }
 
