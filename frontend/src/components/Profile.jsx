25d59d672375496ae3526165f2ba49562c4b7c43
Anwar-Abdel <anwarabdeljaber0@gmail.com>
2024-07-23 21:48:02 -0500

    feat: added styling for profile page and profile route

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,43 +1,42 @@
-import {useState, useEffect} from "react";
+// import {useState, useEffect} from "react";
 import Favorites from "./Favorites";
 import Navbar from "./Navbar";
-import axios from 'axios';
+// import axios from 'axios';
+import '../App.css';
+
 
 const Profile = () => {
 
-    const [user, setUser] = useState({})
+    // const [user, setUser] = useState({})
 
-    useEffect(() => {
-        axios.get('http://localhost:3000/profile')
-        .then(res => {
-            setUser(res.data);
-        })
-        .catch(err => {
-            console.log('error: ', err);
-        })
-    });
+    // useEffect(() => {
+    //     axios.get('http://localhost:3000/profile')
+    //     .then(res => {
+    //         setUser(res.data);
+    //     })
+    //     .catch(err => {
+    //         console.log('error: ', err);
+    //     })
+    // });
     
     
     return(
         <>
             <div>
                 <div>
-                    <Navbar />
-                </div>
+                <h1 className="profile-title">Welcome to your profile!</h1>
 
-                <div>
-                    <h1>Welcome to your profile!</h1>
                 </div>
-                <div>
+                {/* <div>
                     <h3>Username: ${user.username}</h3>
                     <h3>Email: ${user.email}</h3>
-                </div>
+                </div> */}
                 <div>
                     {/*Profile pic here if time allows*/}
                 </div>
-                <div>
+                {/* <div>
                    <Favorites />
-                </div>
+                </div> */}
                 <dov>
                     {/*Reviews component here*/}
                 </dov>
