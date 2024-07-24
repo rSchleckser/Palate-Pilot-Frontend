c5dd12434d086516db9f75b3aa73acaa08b43a1f
rSchleckser <rickyschleckser@gmail.com>
2024-07-23 21:56:16 -0700

    feat: remove services

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Login from './components/Login';
 import SignUp from './components/signUp';
 import Review from './components/Review';
 import Meals from './components/Meals';
-import * as palatePilotService from './services/palatePilotService';
 import './App.css';
 
 function App() {
